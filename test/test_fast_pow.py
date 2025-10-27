296120546d04a242dc21342113fed7641dc46a9b
Simonov Kirill <147266661+NewsPGG@users.noreply.github.com>
2025-10-27 16:47:06 +0300

    Добавление проверки отрицательной и нулевой степени

--- test/test_fast_pow.py
+++ test/test_fast_pow.py
@@ -7,3 +7,9 @@ def test_two_power_two():
 
 def test_negative():
     assert fastPow(-1, 4) == 1
+
+def test_negative_pow():
+    assert fastPow(3, -2) == 1/9
+
+def test_zero_pow():
+    assert fastPow(100, 0) == 1
