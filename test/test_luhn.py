f12fb960607d383caaa316b0efeb639fbdab782d
Simonov Kirill <147266661+NewsPGG@users.noreply.github.com>
2025-10-27 16:47:38 +0300

    Добавление проверок цифр на четных позициях

--- test/test_luhn.py
+++ test/test_luhn.py
@@ -2,8 +2,14 @@ from src.luhn import luhnСheck
 
 
 def test_good():
-    assert luhnСheck("8571 2612 1234 5467")
+    assert luhnСheck("8571 2612 1234 5427")
 
 
 def test_bad():
     assert not luhnСheck("4561 2612 1234 5463")
+
+def test_good_even_positions_numbers():
+    assert luhnСheck("9191 9191 9191 9194")
+
+def test_bad_even_positions_numbers():
+    assert not luhnСheck("9191 9191 9191 9191")
